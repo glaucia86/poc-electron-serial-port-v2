d4ef47a387af59d625bf0846cf4b7412398b55e4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-31 15:58:27 -0300

    Alteração no arquivo 'package.json'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,29 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "globals": {
+    $: true,
+    "window": true,
+    "document": true
+  },
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "no-console": "off",
+    "import/newline-after-import": "off",
+    "global-require": "off",
+    "import/no-unresolved": "off",
+    "no-param-reassign": "off",
+    "no-shadow": "off",
+    "import/extensions": "off",
+    "no-multi-assign": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": true
+      }
+    ],
+    "no-unused-vars": 0,
+    "max-len": 0
+  }
 };
