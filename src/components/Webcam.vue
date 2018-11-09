4b6591b6669cc7feca72b8a4e7eb90404c57d7b6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-11-09 21:28:03 -0200

    criacao de uma nova rota: WebCam

--- src/components/Webcam.vue
+++ src/components/Webcam.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="webcam">
+  <div class="webcam">
     <h1>Esta é a Página Webcam</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
   </div>
