c1adc9043112965a618b9aaaac92eae1a88b39ae
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-11-12 20:00:18 -0200

    Desenvolvimento do index.vue para a webcam

--- src/components/pages/WebCamera/WebCamera.js
+++ src/components/pages/WebCamera/WebCamera.js
@@ -4,15 +4,3 @@
  * Descrição: arquivo responsável por tratar a lógica do 'WebCamera.js'
  * Author: Glaucia Lemos
  */
-
-import Vue from "vue";
-import WebCamera from "webcamjs";
-
-let enabled = false;
-
-export default {
-  name: "WebCamera",
-  components: {},
-  data() {},
-  methods: {}
-};
--- src/components/pages/WebCamera/index.vue
+++ src/components/pages/WebCamera/index.vue
@@ -1,15 +1,41 @@
 <template>
 <div class="webcam">
-   <div class="container">
-      <div class="col-lg-12">
-         <h1>Esta é a Página Webcam</h1>
-         <p>Testando o pacote: WebcamJs:</p>
-         <div class="camera" id="camdemo"></div>
-         <br />
-         <div class="text-center">
-            <input type="button" class="btn btn-primary" id="start" value="Ligar | Desligar Camera">
+   <div class="row">
+     <div class="col-md-6">
+       <h2>WebCam</h2>
+       <!-- Usando 'v-if' com intuito de identificar as câmeras disponíveis na aplicação -->
+       <code v-if="device">{{ device.label }}</code>
+       <div class="border">
+         <web-cam
+            width="100%"
+            :deviceId="deviceId"
+            @started="onStarted"
+            @stopped="onStopped"
+            @error="onError"
+            @cameras="onCameras"
+            @camera-change="onCameraChange"/>
+       </div>
+
+       <div class="row">
+         <div class="col-md-12">
+           <select v-model="camera">
+             <option value="">-- Escolha a Câmera --</option>
+             <option v-for="device in devices" :key="device.deviceId" :value="device.deviceId">{{ device.label }}</option>
+           </select>
          </div>
-      </div>
+         <div class="col-md-12">
+           <button type="button" class="btn btn-primary" @click="onCapture">Tirar Foto</button>
+           <button type="button" class="btn btn-danger" @click="onStop">Parar Câmera</button>
+           <button type="button" class="btn btn-success" @click="onStart">Iniciar Câmera</button>
+         </div>
+       </div>
+     </div>
+     <div class="col-md-6">
+       <h2>Capturar Foto</h2>
+       <figure class="figure">
+         <img :src="img" class="img-responsive">
+       </figure>
+     </div>
    </div>
 </div>
 </template>
