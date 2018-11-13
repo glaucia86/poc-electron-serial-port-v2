d0f132d3129623ae1a00d53afa1ff680683eafc9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-11-13 19:37:51 -0200

    Desenvolvimento inerente a câmera do electron.

--- src/components/pages/WebCamera/WebCamera.js
+++ src/components/pages/WebCamera/WebCamera.js
@@ -5,14 +5,68 @@
  * Author: Glaucia Lemos
  */
 
-import Vue from "vue";
+import { WebCam } from "vue-web-cam"
+import { find, head } from "lodash";
+
 
 export default {
-  name: "WebCamera",
+  name: "app",
+  components: {
+    WebCam
+  },
   data() {
     return {
-      message: ""
+      img: null,
+      camera: null,
+      deviceId: null,
+      devices: []
     };
   },
-  methods: {}
+  computed: {
+    device: function() {
+      return find(this.devices, n => n.deviceId == this.deviceId);
+    }
+  },
+  watch: {
+    camera: function(id) {
+      this.deviceId = id;
+    },
+    devices: function() {
+      // Once we have a list select the first one
+      let first = head(this.devices);
+      if (first) {
+        this.camera = first.deviceId;
+        this.deviceId = first.deviceId;
+      }
+    }
+  },
+  methods: {
+    onCapture() {
+      this.img = this.$refs.webcam.capture();
+    },
+    onStarted(stream) {
+      console.log("On Started Event", stream);
+    },
+    onStopped(stream) {
+      console.log("On Stopped Event", stream);
+    },
+    onStop() {
+      this.$refs.webcam.stop();
+    },
+    onStart() {
+      this.$refs.webcam.start();
+    },
+    onError(error, stream) {
+      console.log("On Error Event", error, stream);
+    },
+    onCameras(cameras) {
+      this.devices = cameras;
+      console.log("On Cameras Event", cameras);
+    },
+    onCameraChange(deviceId) {
+      this.deviceId = deviceId;
+      this.camera = deviceId;
+      console.log("On Camera Change Event", deviceId);
+    }
+  }
 };
