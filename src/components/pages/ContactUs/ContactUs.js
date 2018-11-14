c6b5f43d3a55431c0358aea3d498248f6c8c9086
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-11-14 19:34:00 -0200

    Alteração significativa no escopodo projeto.

--- src/components/pages/ContactUs/ContactUs.js
+++ src/components/pages/ContactUs/ContactUs.js
@@ -5,8 +5,6 @@
  * Author: Glaucia Lemos
  */
 
-import Vue from "vue";
-
 export default {
   name: "ContactUs",
   data() {
@@ -17,8 +15,46 @@ export default {
   },
   methods: {
     PrintTextAreaContent: function() {
-      this.printing = true;
-      print();
+      const { spawn } = require("child_process");
+      const fs = require("fs");
+      const fileName = "out.txt";
+
+      fs.writeFile(fileName, this.textarea_field, err => {
+        if (err) {
+          return console.log("error writing file", err);
+        }
+
+        const child = spawn("lpr", [
+          "-P",
+          "BK-C310-U-1",
+          "-o landscape",
+          "-o PageSize=w68h252",
+          "-o CutOptions=ChainMarks",
+          "-o page-left=3",
+          "-o page-right=3",
+          "-o page-top=5",
+          "-o page-bottom=5",
+          "out.txt"
+        ]);
+
+        child.stdout.on("end", data => {
+          removeFile(fileName, () => {
+            console.log("Successfully printed!");
+          });
+        });
+
+        child.stdout.on("error", data => {
+          removeFile(fileName, () => {
+            console.log("err printing ", data);
+          });
+        });
+      });
+
+      const removeFile = (fileName, cb) => {
+        fs.unlink(fileName, err => {
+          cb();
+        });
+      };
     }
   }
 };
